03ab1e51dbb1fe46f559b8d52e1108f71a83883f
Douglas Marques <douglas.bmarques@hotmail.com>
2024-11-23 10:41:29 -0300

    add template structure

---
+++ codeismine-front/src/routes/vault/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+  import Fa from 'svelte-fa'
+
+  import { faVault, faUser, faKey, faClock } from '@fortawesome/free-solid-svg-icons'
+
+  import { Tooltip, Button, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+
+  let passwords = [
+    {
+      id: 1,
+      name: 'Apple MacBook Pro 17"',
+      color: 'Silver',
+      category: 'Laptop',
+      price: '$2999',
+    },
+    {
+      id: 2,
+      name: 'Microsoft Surface Pro',
+      color: 'White',
+      category: 'Laptop PC',
+      price: '$1999',
+    },
+    {
+      id: 3,
+      name: 'Magic Mouse 2',
+      color: 'Black',
+      category: 'Accessories',
+      price: '$99',
+    },
+  ];
+</script>
+
+<Table hoverable={true}>
+  <TableHead>
+    <TableHeadCell>Name</TableHeadCell>
+    <TableHeadCell></TableHeadCell>
+    <TableHeadCell></TableHeadCell>
+    <TableHeadCell></TableHeadCell>
+    <TableHeadCell></TableHeadCell>
+    <TableHeadCell>
+      <span class="sr-only">Edit</span>
+    </TableHeadCell>
+  </TableHead>
+  <TableBody tableBodyClass="divide-y">
+    {#each passwords as password}
+      <TableBodyRow>
+        <TableBodyCell>{password.name}</TableBodyCell>
+        <TableBodyCell>
+          <Tooltip triggeredBy='#show-credentials'>
+            Show/edit username and password
+          </Tooltip>
+          <Button id='show-credentials'>
+            <Fa icon={faVault}/>
+          </Button>
+        </TableBodyCell>
+        <TableBodyCell>
+          <Tooltip triggeredBy='#copy-username'>
+            Copy username
+          </Tooltip>
+          <Button id='copy-username'>
+            <Fa icon={faUser}/>
+          </Button>
+        </TableBodyCell>
+        <TableBodyCell>
+          <Tooltip triggeredBy='#copy-password'>
+            Copy password
+          </Tooltip>
+          <Button id='copy-password'>
+            <Fa icon={faKey}/>
+          </Button>
+        </TableBodyCell>
+        <TableBodyCell>
+          <Tooltip triggeredBy='#copy-totp'>
+            Copy TOTP
+          </Tooltip>
+          <Button id='copy-totp' disabled>
+            <Fa icon={faClock}/>
+          </Button>
+        </TableBodyCell>
+        <TableHeadCell>
+          <span class="sr-only">Edit</span>
+        </TableHeadCell>
+      </TableBodyRow>
+    {/each}
+  </TableBody>
+</Table>
